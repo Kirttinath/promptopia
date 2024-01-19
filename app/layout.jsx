3865b69436b16c7268ba90cd106a248b72640d9b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-20 00:23:45 +0530

    Navigation and some home page layout added to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
@@ -6,14 +8,19 @@ export const metadata = {
 };
 const RootLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-        <main className="app">{children}</main>
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <body>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </body>
+      </html>
+    </>
   );
 };
 
