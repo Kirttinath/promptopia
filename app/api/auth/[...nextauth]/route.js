f8c63843f47e8c560b23e55afe2fdca2f838f27a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-20 14:44:26 +0530

    callbacks added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,37 +10,39 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    //* store the user id from MongoDB to session
-    const sessionUser = await User.findOne({ email: session.user.email });
-    session.user.id = sessionUser._id.toString();
+  callbacks: {
+    async session({ session }) {
+      //* store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      //! serverless route -> lambda -> dynamodb
-      await connectToDB();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        //! serverless route -> lambda -> dynamodb
+        await connectToDB();
 
-      //* check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email,
-      });
-
-      //* if not , themm create a new user
-      if (!userExists) {
-        await User.create({
+        //* check if a user already exists
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
         });
-      }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        //* if not , themm create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 export { handler as GET, handler as POST };
